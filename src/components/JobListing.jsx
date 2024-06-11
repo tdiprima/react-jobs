d7e9377551f391d0eea2e2cf4290a45046716f09
Tammy DiPrima <tammy.diprima@stonybrook.edu>
2024-06-11 16:34:37 -0400

    react-icons

--- src/components/JobListing.jsx
+++ src/components/JobListing.jsx
@@ -1,6 +1,5 @@
 import { useState } from 'react';
 import { FaMapMarker } from 'react-icons/fa';
-import { Link } from 'react-router-dom';
 
 const JobListing = ({ job }) => {
     const [showFullDescription, setShowFullDescription] = useState(false);
@@ -38,12 +37,12 @@ const JobListing = ({ job }) => {
                         <FaMapMarker className='inline text-lg mb-1 mr-1' />
                         {job.location}
                     </div>
-                    <Link
-                        to={`/jobs/${job.id}`}
+                    <a
+                        href={`/jobs/${job.id}`}
                         className="h-[36px] bg-indigo-500 hover:bg-indigo-600 text-white px-4 py-2 rounded-lg text-center text-sm"
                     >
                         Read More
-                    </Link>
+                    </a>
                 </div>
             </div>
         </div>
